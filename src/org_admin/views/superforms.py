44bd1fa4b5195bc53bb4739294e79760c42ecaee
Uzair <36827239+DottiestDuke47@users.noreply.github.com>
2025-04-08 16:33:59 +0100

    superuser auth check

--- src/org_admin/views/superforms.py
+++ src/org_admin/views/superforms.py
@@ -1,10 +1,20 @@
 from forms.models import SuperForm, Form
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from opportunities.models import Opportunity
 from commonui.views import check_if_hx
 
+def check_superuser(request):
+    if request.user:
+        if request.user.is_superuser:
+            return True
+        else:
+            return HttpResponseRedirect('/org_admin/login/')
+
 def superforms(request, error=None, success=None):
+    
+    check_superuser(request)
+    
     context = {
         "hx": check_if_hx(request),
         "superforms": SuperForm.objects.all(),
@@ -14,6 +24,9 @@ def superforms(request, error=None, success=None):
     return render(request, 'org_admin/superforms/manage_superforms.html', context=context)
 
 def new_superform(request):
+    
+    check_superuser(request)
+    
     if request.method == 'POST':
         # Handle form submission
     
@@ -59,6 +72,8 @@ def new_superform(request):
         return edit_superform(request)
 
 def edit_superform(request, superform_id=None):
+    
+    check_superuser(request)
     
     if request.method == "POST":
         # Handle form submission
