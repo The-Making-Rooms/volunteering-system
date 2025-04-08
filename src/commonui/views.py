44fcd9c1c5e39f014debe842f7e3c993f224ccfb
Uzair <36827239+DottiestDuke47@users.noreply.github.com>
2025-04-08 19:03:40 +0100

    bugfix

--- src/commonui/views.py
+++ src/commonui/views.py
@@ -89,7 +89,7 @@ def index(request):
             return HttpResponseRedirect("/org_admin")
     
     template = loader.get_template("commonui/index.html")
-    orgs = Organisation.objects.filter(featured=True, active=True)
+    orgs = Organisation.objects.filter(featured=True)
     opps = Opportunity.objects.filter(featured=True, active=True)
     org_objects = []
 
