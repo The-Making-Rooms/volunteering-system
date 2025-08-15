a2b969d9b4db930fc4759fc8f86433a6093c2f0a
Uzair Patel <36827239+DottiestDuke47@users.noreply.github.com>
2025-08-15 14:05:32 +0100

    Update 0012_volunteershift_rsvp_reason.py

--- src/rota/migrations/0012_volunteershift_rsvp_reason.py
+++ src/rota/migrations/0012_volunteershift_rsvp_reason.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='volunteershift',
             name='rsvp_reason',
-            field=models.CharField(blank=True, null=True),
+            field=models.CharField(max_length=255, blank=True, null=True),
         ),
     ]
