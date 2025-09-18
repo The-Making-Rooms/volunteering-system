3fcc09ad38d97d28acffc2c72c1b251ca12ed11f
Uzair Patel <36827239+DottiestDuke47@users.noreply.github.com>
2025-09-18 23:05:44 +0530

    Update login_as.py

--- src/org_admin/views/login_as.py
+++ src/org_admin/views/login_as.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.core.exceptions import PermissionDenied
 @login_required
 def login_as(request, user_id):
@@ -13,5 +13,8 @@ def login_as(request, user_id):
     user = get_object_or_404(User, id=user_id)
 
     if user:
-        login(user)
+        logout(request)
+        login(request, user=user)
+
+    return HttpResponseRedirect("/volunteer/")
 
